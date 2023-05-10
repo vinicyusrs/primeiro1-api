0850fed8028bab5c5726f616ee901abf2d58667e
vinicyusrs <vinicyus.rs@gmail.com>
2023-05-10 19:37:47 -0300

    commit inicial

---
+++ src/main/java/com/vinicyus/primeiro1/Pimeiro1Application.java
@@ -0,0 +1,13 @@
+package com.vinicyus.primeiro1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pimeiro1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pimeiro1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinicyus/primeiro1/Pimeiro1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinicyus.primeiro1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pimeiro1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
